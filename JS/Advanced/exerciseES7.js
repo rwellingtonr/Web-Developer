b997199ebae6c4087a3d0d53f55961f4ee413d7b
Wellington Leardini Ramos <73858173+rwellingtonr@users.noreply.github.com>
2021-03-08 09:59:15 -0300

    8.03.21

---
+++ JS/Advanced/exerciseES7.js
@@ -0,0 +1,25 @@
+// Solve the below problems:
+
+
+// #1) Check if this array includes the name "John".
+const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+dragons.includes('John')
+//return False
+
+
+// #2) Check if this array includes any name that has "John" inside of it. If it does, return that
+// name or names in an array.
+const dragons2 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+dragons2.filter(name => name.includes('John') )
+//in this case return 'Johnathan' because this name contains John
+
+
+// #3) Create a function that calulates the power of 100 of a number entered as a parameter
+const power100 = (num) => num**100
+const x = power100(5)
+console.log(x)
+
+// #4) Useing your function from #3, put in the paramter 10000. What is the result?
+// Research for yourself why you get this result
+const y = power100(10000)
+console.log(y)
---
+++ JS/Advanced/exerciseES8.js
@@ -0,0 +1,36 @@
+// Solve the below problems:
+
+// #1) Line up the Turtle and the Rabbit at the start line:
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+const x = 9
+turtle = turtle.padStart(x)
+rabbit = rabbit.padStart(x)
+
+// it should look like this:
+'     ||<- Start line'
+'       🐢'
+'       🐇'
+
+// when you do:
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+//Remove the black spaces and add a padEnd "=" 9 times
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
+Object.entries(obj).map(val => val.join(" ")).join(' ')
