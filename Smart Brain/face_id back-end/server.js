95a69cdd9af1370812e19223dce6dfcb08c5a80e
Wellington Leardini Ramos <73858173+rwellingtonr@users.noreply.github.com>
2021-07-05 22:35:59 -0300

    5/7

--- Smart Brain/face_id back-end/server.js
+++ Smart Brain/face_id back-end/server.js
@@ -8,95 +8,74 @@
 
 */
 
-import express from "express"
+import express from "express";
+import cors from "cors";
 
-const app = express()
-const database = {
-	users: [
-		{
-			name: "Wellington",
-			id: 123,
-			email: "wellingtonTES@gmail.com",
-			password: "blablabla",
-			entries: 0,
-			joined: new Date(),
-		},
-		{
-			name: "Ana",
-			id: 454,
-			email: "AnaTES@gmail.com",
-			password: "Test",
-			entries: 0,
-			joined: new Date(),
-		},
-	],
-}
+const app = express();
 
-app.use(express.json())
+app.use(cors());
 
 // Root Page
-app.get("/", (req, res) => {
-	res.send(database.users)
-})
+app.get("/", (req, res) => {});
 //Signin Page
 app.post("/signin", (req, res) => {
-	if (
-		req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password
-	) {
-		res.json("Working Sing In!")
-	} else {
-		res.status(400).json("error to logging in")
-	}
-})
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("Working Sing In!");
+  } else {
+    res.status(400).json("error to logging in");
+  }
+});
 
 //Register Page
 app.post("/register", (req, res) => {
-	const { password, email, name } = req.body
-	database.users.push({
-		name: name,
-		id: 125,
-		email: email,
-		password: password,
-		entries: 0,
-		joined: new Date(),
-	})
+  const { password, email, name } = req.body;
+  database.users.push({
+    name: name,
+    id: 125,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
 
-	res.json(database.users[database.users.length - 1])
-})
+  res.json(database.users[database.users.length - 1]);
+});
 
 //Profile/:userId Page
 app.get("/profile/:id", (req, res) => {
-	const { id } = req.params
-	let found = false
-	database.users.forEach((user) => {
-		if (user.id === parseInt(id)) {
-			found = true
-			return res.json(user)
-		}
-	})
-	if (!found) {
-		res.status(400).json("Couldn't find this user")
-	}
-})
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === parseInt(id)) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Couldn't find this user");
+  }
+});
 
 //Image Page
 app.post("/image", (req, res) => {
-	const { id } = req.body
-	let found = false
-	database.users.forEach((user) => {
-		if (user.id === parseInt(id)) {
-			found = true
-			user.entries++
-			return res.json(user.entries)
-		}
-	})
-	if (!found) {
-		res.status(400).json("Couldn't find this user")
-	}
-})
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === parseInt(id)) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Couldn't find this user");
+  }
+});
 
 //Listen the Server
 app.listen(3000, () => {
-	console.log("App is running!!")
-})
+  console.log("App is running!!");
+});
