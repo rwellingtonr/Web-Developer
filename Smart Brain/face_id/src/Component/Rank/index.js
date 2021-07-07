935663fdddf48229cf8ae2c09761b1354507b995
Wellington Leardini Ramos <73858173+rwellingtonr@users.noreply.github.com>
2021-07-07 01:04:44 -0300

    a

--- Smart Brain/face_id/src/Component/Rank/index.js
+++ Smart Brain/face_id/src/Component/Rank/index.js
@@ -1,16 +1,12 @@
-import React from 'react';
+import React from "react"
 
-const Rank = () => {
+const Rank = (name) => {
   return (
-    <div >
-      <div className='white f3'>
-        {'Wellington, your current rank is...'}
-      </div>
-      <div className='white f1'>
-        {'#5'}
-      </div>
+    <div>
+      <div className="white f3">{"Wellington, your current rank is..."}</div>
+      <div className="white f1">{"#5"}</div>
     </div>
-  );
+  )
 }
 
-export default Rank;
+export default Rank
