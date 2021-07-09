98f892ab5fa32952cc0facb61a54064e4fa34340
Wellington Leardini Ramos <73858173+rwellingtonr@users.noreply.github.com>
2021-07-09 17:32:52 -0300

    log in

--- Smart Brain/face_id back-end/server.js
+++ Smart Brain/face_id back-end/server.js
@@ -33,14 +33,21 @@ const db = kenx({
 app.get("/", (req, res) => {})
 //Signin Page
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json("Working Sing In!")
-  } else {
-    res.status(400).json("error to logging in")
-  }
+  const { email, password } = req.body
+
+  //Check in the user and the password
+  db("login")
+    .where("email", "=", email)
+    .then(async (data) => {
+      const canProceed = bcrypt.compareSync(password, data[0].hash)
+      if (canProceed) {
+        const user = await db("users").where("email", email)
+        return res.json(user[0])
+      } else {
+        res.status(400).json("Check out you email and password")
+      }
+    })
+    .catch(() => res.status(400).json("Wrong credentials!"))
 })
 
 //Register Page
